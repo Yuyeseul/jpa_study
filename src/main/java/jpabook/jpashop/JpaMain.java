1b52aaf7725cdfce2da7d25929f3c006bd74fe57
Yuyeseul <ryuys0308@naver.com>
2025-07-02 19:51:06 +0900

    Squashed 'ex1-jpashop-jpa/' content from commit 7fdc1ba
    git-subtree-dir: ex1-jpashop-jpa
    git-subtree-split: 7fdc1ba98b6e09a9190696486b4a2744078a555e

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+            Book book = new Book();
+            book.setName("jpa");
+            book.setAuthor("ys");
+
+            em.persist(book);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
